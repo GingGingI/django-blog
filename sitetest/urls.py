4718cc456f7caf64fce5c7ecf8f86121ced3d0ea
GingGingI <sma0147@naver.com>
2018-08-26 14:20:41 +0900

    fix code

--- sitetest/urls.py
+++ sitetest/urls.py
@@ -17,8 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path(r'admin/', admin.site.urls),
-    path(r'home/', include('blog.home')),
-
+    path(r'^admin/', admin.site.urls),
+    path(r'', include('blog.home')),
 
 ]
